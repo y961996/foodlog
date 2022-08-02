d23e3b6331978767686f404f9ed3a2a76da72b19
Yunus Atahan <yunus10atahan@gmail.com>
2022-08-02 11:29:18 +0300

    init project

---
+++ src/test/java/com/yunus/foodlog/FoodLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunus.foodlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
