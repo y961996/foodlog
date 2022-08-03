93d944feaaf4f8ee173485b252110d81a1e583b9
Yunus Atahan <yunus10atahan@gmail.com>
2022-08-03 13:45:33 +0300

    add user, post, comment, like entities and ListToStringConverter class

---
+++ src/main/java/com/yunus/foodlog/entities/Post.java
@@ -0,0 +1,42 @@
+package com.yunus.foodlog.entities;
+
+import com.yunus.foodlog.converters.ListToStringConverter;
+import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "post")
+@Data
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    @Column(length = 60)
+    private String title;
+
+    @Column(columnDefinition = "text", length = 140)
+    private String text;
+
+    // TODO: Max 20 Images
+    @Column(name = "image_paths", length = 9999)
+    @Convert(converter = ListToStringConverter.class)
+    private List<String> imagePaths;
+
+    @Column(name = "short_video_path", length = 333)
+    private String shortVideoPath;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+}
---
+++ src/main/java/com/yunus/foodlog/entities/User.java
@@ -0,0 +1,18 @@
+package com.yunus.foodlog.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "\"user\"")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+    private String password;
+}
