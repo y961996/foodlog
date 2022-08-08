d7077fff227b8ef52b4084d949cccf2a2c0ffd52
Yunus Atahan <yunus10atahan@gmail.com>
2022-08-08 12:47:33 +0300

    fix endpoint name

--- src/main/java/com/yunus/foodlog/controllers/CommentController.java
+++ src/main/java/com/yunus/foodlog/controllers/CommentController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/comments")
+@RequestMapping("api/v1/comments")
 @AllArgsConstructor
 public class CommentController {
 
