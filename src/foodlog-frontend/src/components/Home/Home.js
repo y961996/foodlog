6f6cf3632f9365f270d8a824acafb1fe5bccbe19
Yunus Atahan <yunus10atahan@gmail.com>
2022-09-08 14:26:43 +0300

    refresh posts, postForm and some css

--- src/foodlog-frontend/src/components/Home/Home.js
+++ src/foodlog-frontend/src/components/Home/Home.js
@@ -2,18 +2,15 @@ import React from "react";
 import Post from "../Post/Post";
 import {useEffect, useState} from "react";
 import "./Home.scss";
-import {Box, Container} from "@mui/material";
+import PostForm from "../Post/PostForm";
 
 function Home() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [postList, setPostList] = useState([]);
 
-    // TODO: This is an example of how to get the response when you got an error
-    //       Maybe put this in a method and use that to make requests.
-    let responseClone;
-    useEffect(() => {
-        fetch("http://localhost:8080/api/v1/posts")
+    const refreshPosts = () => {
+        fetch("/posts")
             .then(res => {
                 responseClone = res.clone();
                 return res.json();
@@ -33,32 +30,38 @@ function Home() {
                         })
                 }
             )
-    }, []);
+    }
 
-    if(error) {
+    // TODO: This is an example of how to get the response when you got an error
+    //       Maybe put this in a method and use that to make requests.
+    let responseClone;
+    useEffect(() => {
+        refreshPosts();
+    }, [postList]);
+
+    if (error) {
         return <div>Error!!!</div>;
-    } else if(!isLoaded) {
+    } else if (!isLoaded) {
         return <div>Loading...</div>
     } else {
-        return(
+        return (
             //TODO: This container doesn't work properly. (It won't expand with the posts and
             //      posts don't have margins between them.
             <div className="container">
-                <Container fixed>
-                    <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center",
-                               bgcolor: '#cfe8fc', height: '100vh' }}>
-                        {postList.map(post => (
-                            <Post
-                                userId={post.userId}
-                                userName={post.userName}
-                                title={post.title}
-                                text={post.text}
-                                imagePaths={post.imagePaths}
-                                shortVideoPath={post.shortVideoPath}
-                            />
-                        ))}
-                    </Box>
-                </Container>
+                <PostForm userId={1}
+                          userName={"username"}
+                          refreshPosts={refreshPosts} />
+                {postList.map(post => (
+                    <Post
+                        postId={post.id}
+                        userId={post.userId}
+                        userName={post.userName}
+                        title={post.title}
+                        text={post.text}
+                        imagePaths={post.imagePaths}
+                        shortVideoPath={post.shortVideoPath}
+                    />
+                ))}
             </div>
         );
     }
