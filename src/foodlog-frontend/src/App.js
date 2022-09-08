bf30fc60229ad8ac63a50eab8f3774dd411e4f4e
Yunus Atahan <yunus10atahan@gmail.com>
2022-09-08 08:37:34 +0300

    remove comment

--- src/foodlog-frontend/src/App.js
+++ src/foodlog-frontend/src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import User from "./components/User/User";
-//Bölüm6
+
 function App() {
   return (
     <div className="App">
