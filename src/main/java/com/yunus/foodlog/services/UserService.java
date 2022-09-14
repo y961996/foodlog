b61da8b641eb9024aa4f99e7ca980f3a3af43c4e
Yunus Atahan <yunus10atahan@gmail.com>
2022-09-14 14:17:38 +0300

    add method to get user activities, check for null for the user in update method

--- src/main/java/com/yunus/foodlog/services/UserService.java
+++ src/main/java/com/yunus/foodlog/services/UserService.java
@@ -1,11 +1,15 @@
 package com.yunus.foodlog.services;
 
 import com.yunus.foodlog.entities.User;
+import com.yunus.foodlog.repositories.CommentRepository;
+import com.yunus.foodlog.repositories.LikeRepository;
+import com.yunus.foodlog.repositories.PostRepository;
 import com.yunus.foodlog.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +19,9 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final LikeRepository likeRepository;
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
 
     public List<User> getAllUsers() {
         log.info("UserService -> getAllUsers() called");
@@ -34,10 +41,12 @@ public class UserService {
     public User updateOneUserById(Long userId, User newUser) {
         log.info("UserService -> updateOneUserById() called with userId: " + userId + ", newUser: " + newUser.toString());
         Optional<User> user = userRepository.findById(userId);
-        if(user.isPresent()) {
+        if (user.isPresent()) {
             User foundUser = user.get();
-            foundUser.setUserName(newUser.getUserName());
-            foundUser.setPassword(newUser.getPassword());
+            // TODO: Temp control for nullness
+            if (newUser.getUserName() != null) foundUser.setUserName(newUser.getUserName());
+            if (newUser.getPassword() != null) foundUser.setPassword(newUser.getPassword());
+            foundUser.setAvatar(newUser.getAvatar());
             userRepository.save(foundUser);
             return foundUser;
         } else {
@@ -55,8 +64,20 @@ public class UserService {
         return userRepository.findByUserName(userName);
     }
 
-    public Long getCount(){
+    public Long getCount() {
         log.info("UserService -> getCount() called");
         return userRepository.count();
+    }
+
+    public List<Object> getUserActivity(Long userId) {
+        List<Long> postIds = postRepository.findTopByUserId(userId);
+        if (postIds.isEmpty())
+            return null;
+        List<Object> comments = commentRepository.findUserCommentsByPostId(postIds);
+        List<Object> likes = likeRepository.findUserLikesByPostId(postIds);
+        List<Object> result = new ArrayList<>();
+        result.addAll(comments);
+        result.addAll(likes);
+        return result;
     }
 }
