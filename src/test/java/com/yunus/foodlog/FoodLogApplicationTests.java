98180d0721a8ea51860f733742c9f417218a9b88
Yunus Atahan <yunus10atahan@gmail.com>
2022-10-20 20:40:42 +0300

    remove IT for FoodLogApplicationTests class and disable the test

--- src/test/java/com/yunus/foodlog/FoodLogApplicationTests.java
+++ src/test/java/com/yunus/foodlog/FoodLogApplicationTests.java
@@ -1,12 +1,13 @@
 package com.yunus.foodlog;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FoodLogApplicationTests {
 
     @Test
+    @Disabled
     void contextLoads() {
     }
 
